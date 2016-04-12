496dd2e8b4eaa32598026505ca25cebbd020cebc
Fabio Mangione and Shane Huston <pair+fabio+shane@pivotal.io>
2016-04-12 10:48:43 +0100

    Added CORS configuration

--- src/main/groovy/io/pivotal/Application.groovy
+++ src/main/groovy/io/pivotal/Application.groovy
@@ -2,11 +2,27 @@ package io.pivotal
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
-class Application {
+class Application extends WebMvcConfigurationSupport{
 
     static void main(String[] args) {
         SpringApplication.run(Application, args)
     }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+
 }
